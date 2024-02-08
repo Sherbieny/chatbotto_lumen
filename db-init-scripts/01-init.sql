8b1c75e2d370ed255dd573ffbd1634b3d5b84f77
Sherbieny <sherbieny@outlook.com>
2024-02-08 09:52:20 +0900

    Add support for textsearch_ja extension and custom Dockerfile for the database container

--- db-init-scripts/01-init.sql
+++ db-init-scripts/01-init.sql
@@ -1,3 +1,11 @@
 -- 01-init.sql
--- Create a new database named chatbotto
-CREATE DATABASE chatbotto;
+-- Create the chatbotto database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS chatbotto;
+-- Connect to the chatbotto database
+\ c chatbotto;
+-- Check if the textsearch_ja extension exists
+DO $$ BEGIN CREATE EXTENSION IF NOT EXISTS textsearch_ja;
+EXCEPTION
+WHEN duplicate_object THEN -- Do nothing, the extension already exists
+NULL;
+END $$;
